db65a645923ea758cfdea7fb83b52628ee412d82
Luke Mueller <lukemueller8@gmail.com>
2021-11-10 15:27:10 -0600

    changes route param

--- pages/api/cities/[cityId].js
+++ pages/api/cities/[cityId].js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   if (req.method === "GET") {
     try {
       const response = await fetch(
-        `http://api.openweathermap.org/data/2.5/weather?q=${req.query.city}&appid=${key}`
+        `http://api.openweathermap.org/data/2.5/weather?id=${req.query.cityId}&units=imperial&appid=${key}`
       );
       const data = await response.json();
       res.status(200).json({ data });
